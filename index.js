f2c14c7a3d31c991602472c15bca73bd8439fcf1
kamilko250 <freshmultikk21@gmail.com>
2021-02-13 17:25:39 +0100

    Strona z reneratorem liczb

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+
+app.all('/', function (req, res)
+{
+    res.send('Hello world!');
+});
+
+
+app.get('/api/getRandomNumber', function(req,res)
+{
+    res.send({randomNumber: Math.random()});
+});
+
+app.get('api/random/min/:min/max/:max', function (req, res)
+{
+    var min = Number(req.params.min);
+    var max = Number(req.params.max);
+    var randomNumber = Math.random() * max  + min % max ;
+    res.send({randomNumber});
+});
+app.listen(port, ()=> console.log(port));
