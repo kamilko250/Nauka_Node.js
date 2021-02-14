9929cf99a0471fc5727ed0499ae1bdfb257fc11e
kamilko250 <freshmultikk21@gmail.com>
2021-02-14 16:01:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ app.listen(process.env.PORT);
 app.use(express.urlencoded());
 app.use(express.json());
 
+var min = 0;
+var max = 0;
+
 app.post('/', function(request, response){
-    var min = Number(request.body.number.min);
-    var max = Number(request.body.number.max);
+    min = Number(request.body.number.min);
+    max = Number(request.body.number.max);
     var randomNumber =( Math.random() * max) + min;
     res.send({randomNumber});
 });
 
+
+app.get('/', function (req, res)
+{
+    var randomNumber =( Math.random() * max) + min;
+    res.send({randomNumber});
+});
