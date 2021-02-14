3853c6351515a932ab1d3f1088d7c75d433a7f2f
kamilko250 <freshmultikk21@gmail.com>
2021-02-14 15:39:53 +0100

    update

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const express = require('express');
 
 const app = express();
 app.listen(process.env.PORT);
+
+app.use(express.urlencoded());
+app.use(express.json());
+app.post('/', function(request, response){
+    var min = Number(request.body.number.min);
+    var max = Number(request.body.number.max);
+    var randomNumber =( Math.random() * max) + min;
+    res.send({randomNumber});
+});
+
 app.all('/', function (req, res)
 {
     res.send('Hello world!');
 });
-
 
 app.get('/api/getRandomNumber', function(req,res)
 {
