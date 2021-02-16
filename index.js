1af06b9a02c991127398db984bde151e7c677aff
kamilko250 <freshmultikk21@gmail.com>
2021-02-16 14:01:50 +0100

    update

--- index.js
+++ index.js
@@ -2,28 +2,31 @@ const express = require('express');
 var path = require('path');
 const app = express();
 app.listen(8000);
+app.listen(process.env.PORT);
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
-//app.listen(process.env.PORT);
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 
 var min = 0;
 var max = 0;
-var randomNumber =0;
+var randomNumber = 0;
+var randomNum = 0;
 
-app.post('/randomBetween', function(request, response){
+app.post('/randomBetween', function(request, res){
     min = Number(request.body.number.min);
     max = Number(request.body.number.max);
     randomNumber = (Math.random() * max % (max-min) ) + min;
-    res.render('/template.ejs', {number: randomNumber});
+    res.render('mainView.ejs', {numberRnd: randomNum, numberRnd_: randomNumber });
+});
+app.get('/', function (req, res)
+{
+    res.render('mainView.ejs', {numberRnd: randomNum, numberRnd_: randomNumber});
 });
 
-
-app.get('/random', function (req, res)
+app.post('/random', function (req, res)
 {
-    randomNumber = Math.random();
-    res.render('/mainView.ejs', {number: randomNumber});
-});
-app.get('/',function(req,res)
-{
-    res.render('home.ejs');
+    randomNum = Math.random();
+    res.render('mainView.ejs', {numberRnd: randomNum, numberRnd_: randomNumber});
 });
