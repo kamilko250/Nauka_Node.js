19797dd06dfe2ca0bab858bd1cab7eed990481cd
kamilko250 <freshmultikk21@gmail.com>
2021-02-17 21:23:33 +0100

    update

--- index.js
+++ index.js
@@ -1,32 +1,58 @@
-const express = require('express');
 var path = require('path');
+const express = require('express');
+const fs = require('fs');
 const app = express();
-app.listen(8000);
+var fileupload = require("express-fileupload");
+app.use(fileupload());
+//app.listen(8000);
 app.listen(process.env.PORT);
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-
 var min = 0;
 var max = 0;
 var randomNumber = 0;
 var randomNum = 0;
+var randomNumFile = 0;
 
-app.post('/randomBetween', function(request, res){
-    min = Number(request.body.number.min);
-    max = Number(request.body.number.max);
+app.post('/randomBetween', function(req, res){
+    min = Number(req.body.number.min);
+    max = Number(req.body.number.max);
     randomNumber = (Math.random() * max % (max-min) ) + min;
-    res.render('mainView.ejs', {numberRnd: randomNum, numberRnd_: randomNumber });
+    res.render('mainView.ejs', {numberRnd: randomNum, numberRnd_: randomNumber, numberRndFile_: randomNumFile});
 });
 app.get('/', function (req, res)
 {
-    res.render('mainView.ejs', {numberRnd: randomNum, numberRnd_: randomNumber});
+    res.render('mainView.ejs', {numberRnd: randomNum, numberRnd_: randomNumber, numberRndFile_: randomNumFile});
 });
 
 app.post('/random', function (req, res)
 {
     randomNum = Math.random();
-    res.render('mainView.ejs', {numberRnd: randomNum, numberRnd_: randomNumber});
+    res.render('mainView.ejs', {numberRnd: randomNum, numberRnd_: randomNumber, numberRndFile_: randomNumFile});
 });
+app.post("/upload", function(req, res)
+{
+    var file;
+
+     if(!req.files)
+    {
+        res.send("File was not found");
+         return;
+    }
+
+    file = req.files.textfile;  // here is the field name of the form
+    var buffer = file.data;
+    str = buffer.toString('utf-8');
+    var tab = str.split(',');
+    var min1 = Number(tab[0]);
+    var max1 = Number(tab[1]);
+    randomNumFile = (Math.random() * max1 % (max1-min1) ) + min1;
+    console.log(min1);
+    console.log(max1);
+    console.log(randomNumFile);
+    res.render('mainView.ejs', {numberRnd: randomNum, numberRnd_: randomNumber, numberRndFile_: randomNumFile});
+});
+
