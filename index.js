2fe289730e084ba59b8845fe594d329f0ecfcde7
kamilko250 <freshmultikk21@gmail.com>
2021-02-18 19:53:42 +0100

    update

--- index.js
+++ index.js
@@ -11,52 +11,43 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-var min = 0;
-var max = 0;
-var randomNumber = 0;
-var randomNum = 0;
-var randomNumFile = 0;
 
-app.post('/randomBetween', function(req, res){
-    min = Number(req.body.number.min);
-    max = Number(req.body.number.max);
-    randomNumber = (Math.random() * max % (max-min) ) + min;
-    res.render('mainView.ejs', {numberRnd: randomNum, numberRnd_: randomNumber, numberRndFile_: randomNumFile});
-});
 app.get('/',function(req,res) 
 {
     res.render('basicView.ejs');
-})
-/*app.get('/', function (req, res)
-{
-    res.render('mainView.ejs', {numberRnd: randomNum, numberRnd_: randomNumber, numberRndFile_: randomNumFile});
-});*/
-
-app.post('/random', function (req, res)
-{
-    randomNum = Math.random();
-    res.render('mainView.ejs', {numberRnd: randomNum, numberRnd_: randomNumber, numberRndFile_: randomNumFile});
 });
-app.post("/upload", function(req, res)
+app.get('/zad1',function(req,res)
+{
+    res.render('zad1.ejs',{number: 0 });
+});
+app.post('/zad1',function(req,res)
+{
+    min = Number(req.body.number.min);
+    max = Number(req.body.number.max);
+    var randomNumber = (Math.random() * max % (max-min) ) + min;
+    res.render('zad1.ejs',{number: randomNumber });
+});
+app.get('/zad2', function(req,res)
+{
+    res.render('zad2.ejs',{number: 0});
+});
+app.post('/zad2',function(req,res)
 {
     var file;
-
      if(!req.files)
     {
         res.send("File was not found");
          return;
     }
-
     file = req.files.textfile;  // here is the field name of the form
-    var buffer = file.data;
-    str = buffer.toString('utf-8');
+    str = file.data.toString('utf-8');
     var tab = str.split(',');
     var min1 = Number(tab[0]);
     var max1 = Number(tab[1]);
-    randomNumFile = (Math.random() * max1 % (max1-min1) ) + min1;
-    console.log(min1);
-    console.log(max1);
-    console.log(randomNumFile);
-    res.render('mainView.ejs', {numberRnd: randomNum, numberRnd_: randomNumber, numberRndFile_: randomNumFile});
+    randomNum = (Math.random() * max1 % (max1-min1) ) + min1;
+    res.render('zad2.ejs',{number: randomNum});
 });
-
+app.get('/zad3',function(req,res)
+{
+    res.send('zad3 nie zostalo jeszcze dodane');
+});
