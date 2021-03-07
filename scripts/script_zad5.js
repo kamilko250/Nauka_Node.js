9cde252106bb1384586f16b0b32a9d6f6c8c79d2
kamilko250 <freshmultikk21@gmail.com>
2021-03-07 12:04:41 +0100

    Update script_zad5.js

--- scripts/script_zad5.js
+++ scripts/script_zad5.js
@@ -2,6 +2,8 @@ document.querySelector('#leftform').addEventListener('submit', event => {
   axios.post('/zad5', {
     leftmin: document.querySelector('#leftmin').value,
     leftmax: document.querySelector('#leftmax').value,  
+  }).then(res => {
+    document.querySelector('#postResultDiv').innerText = res.data.result
   })
   event.preventDefault()
   return false
