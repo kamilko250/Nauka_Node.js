59cd41f238dfc814b1e77ec59fdfc5b8b9e9c87d
Fresh2054 <freshmultikk21@gmail.com>
2021-05-10 12:48:06 +0200

    update doc

--- routes/zad9.js
+++ routes/zad9.js
@@ -3,18 +3,18 @@ const router = express.Router()
 
 var words  = []
 router.get('/zad9', (req,res)=>{
-    res.render('zad9.ejs', {words: words})
+    res.send({words: words})
 })
 router.post('/zad9', (req, res) => {
     let word = req.body["word"]
     words.push(word)
-    res.render('zad9.ejs', {words: words})
+    res.send({words: words})
 })
 router.post('/zad9/delete', (req, res) => {
     let word = req.body["word"]
     let position = words.indexOf(word)
     words.splice( position, 1)
-    res.render('zad9.ejs', {words: words})
+    res.send({words: words})
 })
 router.post('/zad9/edit', (req, res)=>{
     let word = req.body.word
@@ -24,6 +24,6 @@ router.post('/zad9/editconfirm', (req , res) => {
     let new_word = req.body.word_edit
     let old_word = req.body.word_old
     words[words.indexOf(old_word)] = new_word
-    res.render('zad9.ejs', {words: words}) 
+    res.send({words: words}) 
 })
 module.exports = router
