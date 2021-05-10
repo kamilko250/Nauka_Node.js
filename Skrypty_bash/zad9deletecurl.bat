e2a03491109f6e559f0ab07c86f6432cbd9d9ee5
Fresh2054 <freshmultikk21@gmail.com>
2021-05-10 13:19:13 +0200

    update doc

--- Skrypty_bash/zad9deletecurl.bat
+++ Skrypty_bash/zad9deletecurl.bat
@@ -1,2 +1,2 @@
-curl -X POST "127.0.0.1:8080/zad9/delete" -H "Content-Type: application/json" -d "{\"word\":\"89\"}"
+curl -X POST "www.fresh-techdev.online/delete" -H "Content-Type: application/json" -d "{\"word\":\"89\"}"
 Pause
--- Skrypty_bash/zad9editconfcurl.bat
+++ Skrypty_bash/zad9editconfcurl.bat
@@ -1,2 +1,2 @@
-curl -X POST "127.0.0.1:8080/zad9/editconfirm" -H "Content-Type: application/json" -d "{\"word_edit\":\"1234\",\"word_old\":\"89\"}"
+curl -X POST "www.fresh-techdev.online/editconfirm" -H "Content-Type: application/json" -d "{\"word_edit\":\"1234\",\"word_old\":\"89\"}"
 Pause
