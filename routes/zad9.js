2867513dd47508b85cfa562978bdf6e2d1986fbb
Fresh2054 <freshmultikk21@gmail.com>
2021-05-10 17:01:35 +0200

    more restful

--- routes/zad9.js
+++ routes/zad9.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 var words  = []
-router.get('/zad9', (req,res)=>{
+router.get('/zad9', (req, res) => {
     res.send({words: words})
 })
 router.post('/zad9', (req, res) => {
@@ -10,20 +10,14 @@ router.post('/zad9', (req, res) => {
     words.push(word)
     res.send({words: words})
 })
-router.post('/zad9/delete', (req, res) => {
+router.delete('/zad9', (req, res) => {
     let word = req.body["word"]
     let position = words.indexOf(word)
     words.splice( position, 1)
     res.send({words: words})
 })
-router.post('/zad9/edit', (req, res)=>{
-    let word = req.body.word
-    res.render('zad9edit.ejs', {word: word})
-})
-router.post('/zad9/editconfirm', (req , res) => {
-    let new_word = req.body.word_edit
-    let old_word = req.body.word_old
-    words[words.indexOf(old_word)] = new_word
-    res.send({words: words}) 
+router.put('/zad9', (req, res) => {
+    words[indexOf(req.query["old"])] = req.query["new"]
+    res.send({words: words})
 })
 module.exports = router
