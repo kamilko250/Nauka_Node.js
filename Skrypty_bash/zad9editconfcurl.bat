baa07741acab1becaf73ab596984388ef3eb0793
Fresh2054 <freshmultikk21@gmail.com>
2021-05-10 11:52:00 +0200

    docs

--- Skrypty_bash/zad9editconfcurl.bat
+++ Skrypty_bash/zad9editconfcurl.bat
@@ -1,1 +1,2 @@
-curl -X POST "127.0.0.1:8080/zad9/edit" -H "Content-Type: application/json" -d "{\"word_edit\":\"1234\",\"word_old\":\"89\"}"
+curl -X POST "127.0.0.1:8080/zad9/editconfirm" -H "Content-Type: application/json" -d "{\"word_edit\":\"1234\",\"word_old\":\"89\"}"
+Pause
--- Skrypty_bash/zad9editcurl.bat
+++
@@ -1,1 +0,0 @@
-curl -X POST "127.0.0.1:8080/zad9/edit" -H "Content-Type: application/json" -d "{\"word\":\"89\"}"
