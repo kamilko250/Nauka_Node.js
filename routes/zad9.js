15db45dafb53c233054bb8975f5828e171e8c4a3
Fresh2054 <freshmultikk21@gmail.com>
2021-05-11 16:04:22 +0200

    simply

--- routes/zad9.js
+++ routes/zad9.js
@@ -3,32 +3,26 @@ const lodash = require('lodash')
 const router = express.Router()
 const min = 1
 const max = 9999
-class Word {
-    constructor(word){
-        this.word = word
-        this.Id = Math.floor(Math.random() * (max - min)) + min;
-    }
-}
 
-var words  = []
+var DB  = []
 router.get('/zad9', (req, res) => {
-    res.send({words: words})
+    res.send({words: DB})
 })
 router.post('/zad9', (req, res) => {
     let word = req.body["word"]
-    words.push(new Word(word))
-    res.send({words: words})
+    DB.push(new Word(word))
+    res.send({words: DB})
 })
 router.delete('/zad9', (req, res) => {
-    let Id = req.body["Id"]
-    lodash.remove(words, x => x.Id == Id)
-    res.send({words: words})
+    let word = req.body["word"]
+    lodash.remove(DB, x => x == word)
+    res.send({words: DB})
 })
 router.put('/zad9', (req, res) => {
-    let Id = req.body["Id"]
-    let item = lodash.find(words, x => x.Id == Id)
+    let word = req.body["word"]
+    let item = lodash.find(DB, x => x == word)
     if(item != undefined)
         item.word = req.body["new_word"]
-    res.send({words: words})
+    res.send({words: DB})
 })
 module.exports = router
